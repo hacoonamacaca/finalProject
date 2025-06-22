7d4e48192d71c0e22d6af867973d4d46c6b7845c
yifanhoho <puddingho84@gmail.com>
2025-06-22 15:13:37 +0800

    建立 PromotionList 頁面+基本路由設定

---
+++ vue-admin/src/router/index.js
@@ -0,0 +1,20 @@
+// src/router/index.js
+
+import { createRouter, createWebHistory } from 'vue-router'
+import PromotionList from '@/pages/PromotionList.vue'
+
+const routes = [
+    {
+        path: '/promotion',
+        name: 'PromotionList',
+        component: PromotionList
+    },
+    // 其他路由...
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
