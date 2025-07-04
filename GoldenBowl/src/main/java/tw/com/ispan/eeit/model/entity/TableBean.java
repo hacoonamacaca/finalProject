f1c78865acf2c89a2bdf51c5ef47a8593c223bc6
ktl <ktl@ispan.com.tw>
2025-07-04 15:05:14 +0800

    0704firsttime uploadspring

---
+++ GoldenBowl/src/main/java/tw/com/ispan/eeit/model/entity/TableBean.java
@@ -0,0 +1,43 @@
+package tw.com.ispan.eeit.model.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Table(name = "tables")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableBean {
+    @ManyToMany(mappedBy = "tables")
+    private Set<ReservationBean> reservations = new HashSet<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    @Column(name = "FK_store_id")
+    private Integer storeId;
+
+    @Column(name = "quantity")
+    private Integer quantity; // 桌位數量
+
+    @Column(name = "seats")
+    private Integer seats; // 座位數量
+
+    @Column(name = "isActive")
+    private Boolean status; // 桌位狀態
+}
