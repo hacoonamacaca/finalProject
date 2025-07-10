9415d7734acdabfbf000bb894bd7eb9591febb84
Ted <s880454@yahoo.com.tw>
2025-07-10 12:28:09 +0800

    前端整合前的清空

--- vue-cus/src/views/RestaurantPage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <RestaurantTemplate :restaurant="restaurant" />
-</template>
-
-<script setup>
-import RestaurantTemplate from "@/components/KTlu/RestaurantTemplate.vue";
-defineProps(["restaurant"]);
-</script>
